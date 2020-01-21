306ca1fb6819355726ae4d279c016095974aac82
Tolga Gunaydin <gnydnntolga@gmail.com>
2020-01-21 23:01:26 +0300

    Profil sayfası yeniden tasarlandı.

--- web/src/components/Profil.vue
+++ web/src/components/Profil.vue
@@ -2,12 +2,44 @@
 
 <section class="profile">
   <div class="container">
-    <div class="title">
-      <p> Kişisel Bilgilerim </p>
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-12 mt-5">
+        <div>
+          <span class="profile__title"> Kişisel Bilgilerim </span>
+          <span class="profile__edit"> Düzenle </span>
+          <hr>
+        </div>
+        <div class="profile__info">
+          <span class="profile__itemtitle"> E-Postanız: </span>
+          <span class="profile__itemcontent"> tolgagnydnn@gmail.com </span>
+        </div>
+        <div class="profile__info">
+          <span class="profile__itemtitle"> Adınız: </span>
+          <span class="profile__itemcontent"> Tolga </span>
+        </div>
+        <div class="profile__info">
+          <span class="profile__itemtitle"> Soyadınız: </span>
+          <span class="profile__itemcontent"> Günaydın </span>
+        </div>
+        <div class="profile__info">
+          <span class="profile__itemtitle"> Telefon Numaranız: </span>
+          <span class="profile__itemcontent"> 999 999 99 99 </span>
+        </div>
+      </div>
+
+      <div class="col-lg-12 col-md-12 col-sm-12 col-12 mt-5">
+        <div>
+          <span class="profile__title"> Şifre Bilgilerim </span>
+          <span class="profile__edit"> Düzenle </span>
+          <hr>
+        </div>
+        <div class="profile__password">
+          <span class="profile__itemtitle"> Mevcut Şifreniz: </span>
+          <span class="profile__itemcontent"> *******  </span>
+        </div>
     </div>
 
-
-
+   </div>
   </div>
 </section>
 
