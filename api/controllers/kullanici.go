a32ef6be158ff40fb34b4c5e418af8d48e79e470
Zafer Celenk <zafercelenk@gmail.com>
2020-01-25 11:47:34 +0300

    API tarafına kullanıci için bilgi, ekle ve düzenle metodları eklendi.

--- api/controllers/kullanici.go
+++ api/controllers/kullanici.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"api/models"
+	"encoding/json"
+	"fmt"
 
 	"github.com/astaxie/beego"
 )
@@ -11,12 +13,49 @@ type KullaniciController struct {
 	beego.Controller
 }
 
+// Duzenle function
+// @Title Ekle
+// @Description Yeni kullanici bilgisi ekler.
+// @Param body body models.Kullanici true "kullanici model bilgisi"
+// @Success 200 {object} models.Kullanici
+// @router /duzenle [post]
+func (c *KullaniciController) Duzenle() {
+	var kullanici models.Kullanici
+	json.Unmarshal(c.Ctx.Input.RequestBody, &kullanici)
+
+	fmt.Println("--> Kullanici: ", kullanici)
+
+	c.Data["json"] = kullanici
+	c.ServeJSON()
+}
+
+// Ekle function
+// @Title Ekle
+// @Description Yeni kullanici bilgisi ekler.
+// @Param body body models.Kullanici true "kullanici model bilgisi"
+// @Success 200 {object} models.Kullanici
+// @router /ekle [post]
+func (c *KullaniciController) Ekle() {
+	var kullanici models.Kullanici
+	json.Unmarshal(c.Ctx.Input.RequestBody, &kullanici)
+
+	fmt.Println("--> Kullanici: ", kullanici)
+
+	c.Data["json"] = kullanici
+	c.ServeJSON()
+}
+
 // Bilgi function
 // @Title Bilgi
 // @Description Kullanici bilgilerini getirir.
+// @Param id path string true "Kullanici ID bilgisi"
 // @Success 200 {object} models.Kullanici
-// @router / [get]
+// @router /bilgi/:id [get]
 func (c *KullaniciController) Bilgi() {
+	var kullaniciID = c.GetString(":id")
+
+	fmt.Println("--> Kullanici ID: ", kullaniciID)
+
 	var kullanici = models.KullaniciBilgi()
 
 	c.Data["json"] = kullanici
--- api/routers/commentsRouter_controllers.go
+++ api/routers/commentsRouter_controllers.go
@@ -10,8 +10,26 @@ func init() {
     beego.GlobalControllerRouter["api/controllers:KullaniciController"] = append(beego.GlobalControllerRouter["api/controllers:KullaniciController"],
         beego.ControllerComments{
             Method: "Bilgi",
-            Router: `/`,
+            Router: `/bilgi/:id`,
             AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:KullaniciController"] = append(beego.GlobalControllerRouter["api/controllers:KullaniciController"],
+        beego.ControllerComments{
+            Method: "Duzenle",
+            Router: `/duzenle`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["api/controllers:KullaniciController"] = append(beego.GlobalControllerRouter["api/controllers:KullaniciController"],
+        beego.ControllerComments{
+            Method: "Ekle",
+            Router: `/ekle`,
+            AllowHTTPMethods: []string{"post"},
             MethodParams: param.Make(),
             Filters: nil,
             Params: nil})
