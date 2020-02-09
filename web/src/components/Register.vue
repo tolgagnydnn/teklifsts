f7410157166d2439ac140d997138a4ea54a67c9b
Tolga Gunaydin <gnydnntolga@gmail.com>
2020-02-09 13:12:44 +0300

    Kayıt olma işlemi jsonplaceholder apisi ile yapıldı.

--- web/src/components/Register.vue
+++ web/src/components/Register.vue
@@ -9,7 +9,7 @@
                     <i class="fas fa-envelope"> </i>
                   </div>
                 </span>
-                <input  class="registermodal__input" type="email" placeholder="Email Giriniz" />
+                <input  v-model="register.email" class="registermodal__input" type="email" placeholder="Email Giriniz" />
               </div>
             </div>
             <div class="form-group py-2">
@@ -19,7 +19,7 @@
                     <i class="fas fa-user"> </i>
                   </div>
                 </span>
-                <input  class="registermodal__input" type="email" placeholder="Adınızı Giriniz" />
+                <input v-model="register.name" class="registermodal__input" type="text" placeholder="Adınızı Giriniz" />
               </div>
             </div>
             <div class="form-group py-2">
@@ -29,7 +29,17 @@
                     <i class="fas fa-user"> </i>
                   </div>
                 </span>
-                <input  class="registermodal__input" type="email" placeholder="Soyadınızı Giriniz" />
+                <input  v-model="register.surname" class="registermodal__input" type="text" placeholder="Soyadınızı Giriniz" />
+              </div>
+            </div>
+            <div class="form-group py-2">
+              <div class="mb-0 input-group">
+                <span class="registermodal__prepend">
+                  <div class="registermodal__icon">
+                    <i class="fas fa-phone"> </i>
+                  </div>
+                </span>
+                <input  v-model="register.telephone" class="registermodal__input" type="text" autocomplete="off" placeholder="Telefon Numaranızı Giriniz" />
               </div>
             </div>
             <div class="form-group py-2">
@@ -39,7 +49,7 @@
                     <i class="fas fa-lock"> </i>
                   </div>
                 </span>
-                <input  class="registermodal__input" type="password" autocomplete="off" placeholder="Şifrenizi Giriniz" />
+                <input  v-model="register.password" class="registermodal__input" type="password" autocomplete="off" placeholder="Şifrenizi Giriniz" />
               </div>
             </div>
             <div class="form-group py-2">
@@ -49,12 +59,12 @@
                     <i class="fas fa-lock"> </i>
                   </div>
                 </span>
-                <input  class="registermodal__input" type="password" autocomplete="off" placeholder="Şifrenizi Tekrar Giriniz" />
+                <input  v-model="register.passwordagain" class="registermodal__input" type="password" autocomplete="off" placeholder="Şifrenizi Tekrar Giriniz" />
               </div>
             </div>
 
             <div>
-              <button class="registermodal__btn" type="submit"> Üye Ol </button>
+              <button @click.prevent="successRegister" class="registermodal__btn" type="submit"> Üye Ol </button>
             </div>
             <div>
               <button class="registermodal__close" @click="closeregister"> <i class="fas fa-times"> </i> </button>
@@ -66,11 +76,30 @@
 
 
 <script>
-import {eventBus} from '../main'
+import {eventBus} from '../main';
+import axios from 'axios'
 export default {
+  data(){
+    return {
+      register: {
+        email:'',
+        name:'',
+        surname:'',
+        telephone:'',
+        password:'',
+        passwordagain:''
+      }
+    }
+  },
   methods:{
     closeregister(){
       eventBus.$emit('registerclose');
+    },
+    successRegister(){
+      axios.post("http://jsonplaceholder.typicode.com/posts", { ...this.register})
+      .then(response => {
+        console.log(response);
+      })
     }
   }
 }
